89eb06f168f5eb52204b7b83c52a2dcdccf65ebd
Basit Ali <opoali911@gmail.com>
2025-01-20 13:34:52 +0500

    working on the custom slider

--- miniProjects/src/miniProject1/CustomeSlider.jsx
+++ miniProjects/src/miniProject1/CustomeSlider.jsx
@@ -1,52 +1,67 @@
-import React, { useState } from 'react'
-import {dummyProducts} from '../assets/Data.js'
-import {ChevronLeft,ChevronRight,ChevronUp,ChevronDown} from 'react-feather'
+import React, { useState } from "react";
+import { dummyProducts } from "../assets/Data.js";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronUp,
+  ChevronDown,
+} from "react-feather";
 
 const CustomeSlider = () => {
-  const [showMore,setShowMore]=useState(false);
-  const [index,setIndex]=useState(0);
+  const [showMore, setShowMore] = useState(false);
+  const [index, setIndex] = useState(0);
 
-  const handleToggle=()=>{
-    setShowMore(!showMore)
-  }
-  const handleNext=()=>{
-    setIndex((currentIndex)=>(currentIndex+1)%dummyProducts.length)
-  }
-  const handlePrevious=()=>[
-    setIndex((currentIndex)=>(currentIndex-1 + dummyProducts.length)% dummyProducts.length)
-  ]
+  const handleToggle = () => {
+    setShowMore(!showMore);
+  };
+  const handleNext = () => {
+    setIndex((currentIndex) => (currentIndex + 1) % dummyProducts.length);
+  };
+  const handlePrevious = () => [
+    setIndex(
+      (currentIndex) =>
+        (currentIndex - 1 + dummyProducts.length) % dummyProducts.length
+    ),
+  ];
 
-  const product=dummyProducts[index]
-    
+  const product = dummyProducts[index];
+
   return (
     <>
-    <section className='flex flex-col justify-center items-center bg-[#E8F9FF] w-full h-screen gap-2  '>
-       <div className='flex justify-center items-center mt-[-120px] bg-[#C5BAFF] w-[800px] h-[400px]'>
-       <div>
-        <button onClick={handlePrevious}> 
-        <ChevronLeft/>
-        </button>
+      <section className="flex flex-col justify-center items-center bg-[#E8F9FF] w-full h-screen gap-2  ">
+        <div className="flex justify-center items-center gap-52">
+          <div>
+            <button onClick={handlePrevious}>
+              <ChevronLeft />
+            </button>
+          </div>
+          <div className="w-[500px] h-[400px] bg-blue-200" >
+            <div className="flex justify-center items-center">
+              <img className="w-3/4" src={product.images[1]} alt="Product-Image" />
+            </div>
+          </div>
+          <div>
+            <button onClick={handleNext}>
+              <ChevronRight />
+            </button>
+          </div>
         </div>
-        <div className='flex justify-center items-center'>
-            <img className='w-[50%]' src={product.images[3]} alt="Product-Image" />
-        </div>
+
         <div>
-          <button onClick={handleNext}>
-          <ChevronRight/>
-
+          <button onClick={handleToggle}>
+            {showMore ? <ChevronUp /> : <ChevronDown />}
           </button>
+         {
+          showMore && (
+            <div>
+            <p>ProudctName:{product.title}</p>
+          </div>
+          )
+         }
         </div>
-
-       </div>
-       <div>
-        <button onClick={handleToggle}>
-          {showMore?<ChevronUp/>:<ChevronDown/>}
-        </button>
-       </div>
-    </section>
-      
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default CustomeSlider
+export default CustomeSlider;
