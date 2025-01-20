df70b48e88bf8ae1497f01f0ef445aaf03792252
Basit Ali <opoali911@gmail.com>
2025-01-20 23:39:30 +0500

    the mini project is complete

--- miniProjects/src/Projects/TextColorChanger/TextColorChanger.jsx
+++ miniProjects/src/Projects/TextColorChanger/TextColorChanger.jsx
@@ -4,20 +4,20 @@ const TextColorChanger = () => {
   const [colors,setColors]=useState('black')
   return (
     <>
-     <section>
-      <div>
-        <select onChange={(e)=>setColors(e.target.value)}>
-          <option value="red">Red</option>
-          <option value="blue">Blue</option>
-          <option value="green">Green</option>
-          <option value="yellow">Yellow</option>
-          <option value="brown">Brown</option>
-          <option value="pink">Pink</option>
-          <option value="orange">Orange</option>
-          <option value="purple">Purple</option>
+     <section className='w-full h-[75%] my-40'>
+      <div className='flex  justify-evenly items-center '>
+        <select className='w-[25%] p-1' onChange={(e)=>setColors(e.target.value)}>
+          <option className='text-red-500' value="red">Red</option>
+          <option className='text-blue-500' value="blue">Blue</option>
+          <option className='text-green-500' value="green">Green</option>
+          <option className='text-yellow-500' value="yellow">Yellow</option>
+          <option className='text-amber-600' value="brown">Brown</option>
+          <option className='text-pink-500' value="pink">Pink</option>
+          <option className='text-orange-500' value="orange">Orange</option>
+          <option className='text-purple-500' value="purple">Purple</option>
         </select>
-      </div>
     <TextColorChangerChild color={colors}/>
+      </div>
 
     </section> 
     </>
@@ -32,7 +32,7 @@ const TextColorChangerChild=({color})=>{
     <>
     <section>
     <div>
-      <h1 style={{color:color}}>Change the Text Color to select the color from the dropDown</h1>
+      <h1 className='text-3xl font-bold'  style={{color:color}}>Pick the color from the drop down and change color </h1>
       
     </div>
     </section>
