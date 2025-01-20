1b88163f8a464cb9203aa5fb2948ddab1f6c8484
Basit Ali <opoali911@gmail.com>
2025-01-20 10:33:18 +0500

    the basic structure

---
+++ miniProjects/src/assets/miniProject1/CustomeSlider.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const CustomeSlider = () => {
+    
+  return (
+    <>
+    <section>
+       <div>
+       <div>
+            <button>Prev</button>
+        </div>
+        <div>
+            <img src={} alt="" />
+        </div>
+        <div>
+            <button>Nexr</button>
+        </div>
+
+       </div>
+       <div>
+        <button> Detail</button>
+       </div>
+    </section>
+      
+    </>
+  )
+}
+
+export default CustomeSlider
