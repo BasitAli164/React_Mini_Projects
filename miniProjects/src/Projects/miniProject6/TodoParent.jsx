77bff154c8ab638ea7d16f6b0ed31b75fcd86b11
Basit Ali <opoali911@gmail.com>
2025-01-22 00:37:00 +0500

    working on the todo mini project

--- miniProjects/src/Projects/miniProject6/TodoParent.jsx
+++ miniProjects/src/Projects/miniProject6/TodoParent.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const initailTodos=[
+    {id:0,title:'Wake Early Morning',done:true},
+    {id:1,title:'Give Tution for Sister',done:true},
+    {id:3,title:"Go for Charging ",done:true},
+]
 
 const TodoParent = () => {
+    const [todos,setTodos]=useState(initailTodos);
   return (
     <>
       
