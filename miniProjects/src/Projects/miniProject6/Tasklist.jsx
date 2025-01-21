3b8092853259c842c001ce97d6000fde6c3769b1
Basit Ali <opoali911@gmail.com>
2025-01-22 00:32:00 +0500

    working on the todo mini project

--- miniProjects/src/Projects/miniProject6/Tasklist.jsx
+++ miniProjects/src/Projects/miniProject6/Tasklist.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Tasklist = ({todos,onChangeTodo, onDeleteTodo}) => {
   return (
     <>
-      
+      <ul>
+        {
+            todos.map(todo=>(
+                <li key={todo.id}>
+                    <Task
+                    todo={todo}
+                    onChange={onChangeTodo}
+                    onDelete={onDeleteTodo}
+                    />
+                </li>
+            ))
+        }
+      </ul>
     </>
   )
 }
 
 export default Tasklist
+
+const Task=({todo,onChange,onDelete})=>{
+    const [isEditing,setIsEditing]=useState(false)
+    let todoContent;
+    if(isEditing){
+        todoContent=(
+            <>
+            <input type="text" value={todo.title} onChange={(e)=>onChange({
+                ...todo,
+                title:e.target.value
+
+                
+            })} />
+            <button onClick={()=>setIsEditing(false)}>
+                Save
+            </button>
+            </>
+        );
+    
+    }else{
+        todoContent=(
+            <>
+            {todo.title}
+            <button onClick={()=>setIsEditing(true)}>Edit</button>
+            </>
+        )
+    }
+    return(
+        <>
+        <label >
+            <input type="checkbox"  checked={todo.done} onChange={(e)=>{
+                onChange({
+                    ...todo,
+                    done:e.target.value
+                })
+            }} />
+            {todoContent}
+        </label>
+        </>
+    )
+}
