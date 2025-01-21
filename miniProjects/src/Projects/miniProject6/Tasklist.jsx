c2cb04b656587e5027f00c529a87384d28c7c911
Basit Ali <opoali911@gmail.com>
2025-01-22 00:17:55 +0500

    know work the todo project

---
+++ miniProjects/src/Projects/miniProject6/Tasklist.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Tasklist = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default Tasklist
---
+++ miniProjects/src/Projects/miniProject6/TodoParent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoParent = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default TodoParent
