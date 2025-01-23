7690bf1a913bf4d6efac309f22222289d53e02bf
Basit Ali <opoali911@gmail.com>
2025-01-23 22:53:03 +0500

    coming to work on the parent

--- miniProjects/src/Projects/miniProject7/Contact.jsx
+++ miniProjects/src/Projects/miniProject7/Contact.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import EditContact from './EditContact';
+const initailContact=[
+    {id:1,name:"Basit Ali",email:"Abdupa911@gmail.com"},
+    {id:2,name:"Muhammad Ali",email:"mali@gmail.com"},
+    {id:3,name:"Sakina Batool",email:"sakina@gmail.com"},
+    {id:4,name:"Khatija Ali",email:"khatija@gmail.com"},
+    {id:5,name:"Kubra Batool",email:"kubra@gmail.com"},
+
+]
 
 const Contact = () => {
+    const [contact,setContact]=useState(initailContact);
+    const [selectedId,setSelectedId]=useState(1)
+
+    const selectedContact=contact.find((contactId)=>contactId.id===selectedId)
+
+    function  handleSave(){
+
+    }
   return (
     <>
+    <div>
+        <ContactList/>
+        <hr />
+        <EditContact/>
+    </div>
       
     </>
   )
