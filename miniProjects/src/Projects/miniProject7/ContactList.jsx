ffeaa24fa0bad32dbb73826f03d1065700764c83
Basit Ali <opoali911@gmail.com>
2025-01-23 22:43:53 +0500

    start the mini project

--- miniProjects/src/Projects/miniProject7/ContactList.jsx
+++ miniProjects/src/Projects/miniProject7/ContactList.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
+const initailContact=[
+    {id:1,name:"Basit Ali",email:"Abdupa911@gmail.com"},
+    {id:2,name:"Muhammad Ali",email:"mali@gmail.com"},
+    {id:3,name:"Sakina Batool",email:"sakina@gmail.com"},
+    {id:4,name:"Khatija Ali",email:"khatija@gmail.com"},
+    {id:5,name:"Kubra Batool",email:"kubra@gmail.com"},
+
+]
 
 const ContactList = () => {
+    const [contact,setContact]=useState(initailContact);
+    const [selectedId,setSelectedId]=useState(1)
   return (
     <>
       
