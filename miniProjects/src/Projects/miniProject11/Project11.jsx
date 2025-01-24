08a0bc30fea07082104552a4cb11588f7f93159a
Basit Ali <opoali911@gmail.com>
2025-01-24 22:47:30 +0500

    start new project woth reducer hook

---
+++ miniProjects/src/Projects/miniProject11/Project11.jsx
@@ -0,0 +1,32 @@
+import React, { act, useReducer } from 'react'
+
+const reducer=(state,action)=>{
+    switch(action.type){
+        case 'Increment':{
+            return state+1;
+            
+        }
+        case 'decrement':{
+            return state-1;
+            
+        }
+        default: throw Error("Invalid State")
+    }
+}
+
+let initialState=0;
+const Project11 = () => {
+    const [state,dispatch]=useReducer(reducer,initialState);
+
+  return (
+    <>
+    <button className='text-3xl font-bold mx-3 py-2
+     px-10 rounded-full bg-cyan-300' onClick={()=>dispatch({type:'decrement'})}>-</button>
+    <span className='mx-10 text-5xl font-bold'>{state}</span>
+    <button className='text-3xl font-bold mx-3 py-2 px-10 rounded-full bg-cyan-300'>+</button>
+      
+    </>
+  )
+}
+
+export default Project11
