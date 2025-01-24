a38956f09bf4c45eb6837f1ceb82e215933d93d0
Basit Ali <opoali911@gmail.com>
2025-01-24 11:59:20 +0500

    know start

--- miniProjects/src/Projects/miniProject10/Profile.jsx
+++ miniProjects/src/Projects/miniProject10/Profile.jsx
@@ -3,6 +3,16 @@ import React from 'react'
 const Profile = () => {
   return (
     <div>
+        <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={100}
+          height={100}
+        />
+      </Card>
       
     </div>
   )
@@ -13,7 +23,12 @@ export default Profile
 function Cart({children}){
     return(
         <>
-        
+        <div>
+            <div>
+                {children}
+            </div>
+        </div>
+
         
         </>
     )
