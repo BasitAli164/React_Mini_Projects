b7d463da0fb0db49765cc88c06ad0a42b52d6cbb
Basit Ali <opoali911@gmail.com>
2025-01-24 23:03:32 +0500

    now the mini Project is ready which is a counter and built with reducer

--- miniProjects/src/Projects/miniProject11/Project11.jsx
+++ miniProjects/src/Projects/miniProject11/Project11.jsx
@@ -1,21 +1,18 @@
-import React, {  useReducer } from "react";
+import React, { useReducer } from "react";
 
 const reducer = (state, action) => {
-    console.log(action,state)
+  console.log(action, state);
   switch (action.type) {
     case "Increment": {
-        console.log(typeof state)
-        if(typeof(state)==='number')return state + 1;
-      
+      console.log(typeof state);
+      if (typeof state === "number") return state + 1;
     }
     case "decrement": {
-        if(typeof(state)==='number')return state - 1;
+      if (typeof state === "number") return state - 1;
     }
-    default:{
-        return "Some thing we Wrong"
-
+    default: {
+      return "Some thing we Wrong";
     }
-      
   }
 };
 
@@ -25,7 +22,7 @@ const Project11 = () => {
 
   return (
     <>
-    <h1 className="text-3xl mx-10 font-bold">Counter with Reducer</h1>
+      <h1 className="text-3xl mx-10 font-bold">Counter with Reducer</h1>
       <button
         className="text-3xl font-bold mx-3 py-2
      px-10 rounded-full bg-cyan-300"
@@ -34,10 +31,16 @@ const Project11 = () => {
         -
       </button>
       <span className="mx-10 text-5xl font-bold">{state}</span>
-      <button className="text-3xl font-bold mx-3 py-2 px-10 rounded-full bg-cyan-300" onClick={()=>dispatch({type:"Increment"})}>
+      <button
+        className="text-3xl font-bold mx-3 py-2 px-10 rounded-full bg-cyan-300"
+        onClick={() => dispatch({ type: "Increment" })}
+      >
         +
       </button>
-      <button className="text-3xl font-bold mx-3 py-2 px-10 rounded-full bg-cyan-300" onClick={()=>dispatch({type:"Incremsent"})}>
+      <button
+        className="text-3xl font-bold mx-3 py-2 px-10 rounded-full bg-cyan-300"
+        onClick={() => dispatch({ type: "Incremsent" })}
+      >
         Click me
       </button>
     </>
