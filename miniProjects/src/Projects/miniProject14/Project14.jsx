2f51966c41d63ca38dedcf58b93219767a5a4042
Basit Ali <opoali911@gmail.com>
2025-01-26 17:13:45 +0500

    working on the newmini project

--- miniProjects/src/Projects/miniProject14/Project14.jsx
+++ miniProjects/src/Projects/miniProject14/Project14.jsx
@@ -1,7 +1,7 @@
 import React, { useReducer } from 'react';
 const createInitialState=(userName)=>{
     const initialTodos=[];
-    for(let i=0;i<=50;i++){
+    for(let i=0;i<=20;i++){
         initialTodos.push({
             id:i,
             text:userName+"s task #"+(i+1)
@@ -31,7 +31,17 @@ const Project14 = () => {
     const [state,dispatch]=useReducer(reducer,userName,createInitialState);
   return (
     <>
-      
+      <input type="text" value={state.draft} onChange={(e)=>dispatch({type:'changeDraft',nextDraft:e.target.value})} className='border-[2px] border-solid border-gray-500' />
+      <button className='px-8 py-1 mx--2 rounded-full bg-cyan-400' onClick={()=>{
+        dispatch({type:'addTodo'})
+      }}>Add</button>
+      <ul>
+        {
+            state.todos.map((item)=>(
+                <li key={item.id}>{item.text}</li>
+            ))
+        }
+      </ul>
     </>
   )
 }
