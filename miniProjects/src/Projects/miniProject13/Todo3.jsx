eb37ea31da5f403eae095eb7f2e1fdfedabc0a18
Basit Ali <opoali911@gmail.com>
2025-01-26 13:49:46 +0500

    working on the todo app with reducer funtion

--- miniProjects/src/Projects/miniProject13/Todo3.jsx
+++ miniProjects/src/Projects/miniProject13/Todo3.jsx
@@ -1,15 +1,65 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 let nextId=3;
-initialState=[
+const initialState=[
     {id:1,title:"Reading a book",done:true},
     {id:3,title:"Writing a paragraph",done:false}
 ]
+const reducer=(todo,action)=>{
+  console.log(action)
+  switch(action.type){
+    case "addTodo":{
+      return  [
+        ...todo,
+        {
+          id:nextId++,
+          title:action.payload,
+          done:false,
+        }
+      ]
+      
+    }
+    case "updateTodo":{
+
+    }
+    case 'deleteTodo':{
+
+    }
+    case 'toggle':{
+      return (
+        todo.map((item)=>{
+          item.id==action.payload.id?{...item,done:action.payload.value}:item
+        })
+      )
+
+    }
+    default:{
+      throw Error("Error")
+    }
+  }
+}
 
 const Todo3 = () => {
     const [todo,dispatch]=useReducer(reducer,initialState)
+    const [title,setTitle]=useState('')
     console.log("the initail state is:",todo)
   return (
     <div>
+      <div>
+      <input type="text" value={title} onChange={(e)=>setTitle(e.target.value)} className='border-[2px] border-solid border-black ' />
+      <button className='px-8 py-1 mx-2 rounded-full bg-cyan-500' onClick={()=>dispatch({type:'addTodo',payload:title})}>Add</button>
+      </div>
+      <div>
+        {
+          todo.map((item)=>(
+            <div key={item.id}>
+              <div>
+                <input type="checkbox" checked={item.done} onChange={(e)=>dispatch({type:'toggle',payload:{id:item.id,value:e.target.checked}})} />
+                <span className={`${item.done?'line-through':''}`}>{item.title}</span>
+              </div>
+            </div>
+          ))
+        }
+      </div>
       
     </div>
   )
