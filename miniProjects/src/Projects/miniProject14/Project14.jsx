27580d43611b5048c5e32327e187bcd5fae4e5b6
Basit Ali <opoali911@gmail.com>
2025-01-26 17:03:16 +0500

    working on the new mini project

--- miniProjects/src/Projects/miniProject14/Project14.jsx
+++ miniProjects/src/Projects/miniProject14/Project14.jsx
@@ -1,8 +1,34 @@
-import React, { useReducer } from 'react'
-import {messengerReducer,initialState} from './data'
+import React, { useReducer } from 'react';
+const createInitialState=(userName)=>{
+    const initialTodos=[];
+    for(let i=0;i<=50;i++){
+        initialTodos.push({
+            id:i,
+            text:userName+"s task #"+(i+1)
+        })
+    }
+    return {
+        draft:'',
+        todos:initialTodos
+    }
+}
+
+
+const reducer=(state,action)=>{
+    switch(action.type){
+        case 'changeDraft':{
+            
+        }
+        case 'addTodo':{
+            
+        }
+        default:{
+            throw Error(`Unknown action: ${action.type}`)
+        }
+    }
+}
 const Project14 = () => {
-    const [state,dispatch]=useReducer(messengerReducer,initialState)
-    console.log("initailState: ",state)
+    const [state,dispatch]=useReducer(reducer,userName,createInitialState);
   return (
     <>
       
