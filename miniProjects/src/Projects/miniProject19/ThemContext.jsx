6a495e6dd718751981bd800fb3c01cf9c0eef3bd
Basit Ali <opoali911@gmail.com>
2025-01-27 15:12:08 +0500

    then provider also ready

--- miniProjects/src/Projects/miniProject19/ThemContext.jsx
+++ miniProjects/src/Projects/miniProject19/ThemContext.jsx
@@ -1,3 +1,11 @@
 import { createContext,useContext } from "react";
 
 const themeContext=createContext({});
+
+export const ThemeProvider=({children})=>{
+    return(
+        <themeContext.Provider>
+        {children}
+        </themeContext.Provider>
+    )
+}
