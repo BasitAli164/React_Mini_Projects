f571067273c08307744498fc5b50df3e4250b014
Basit Ali <opoali911@gmail.com>
2025-01-27 15:24:21 +0500

    the reducer is ready

---
+++ miniProjects/src/Projects/miniProject15/messengerReducer.js
@@ -0,0 +1,20 @@
+import { act } from "react";
+
+export const initialState={
+    selectedId:0,
+    message:'Hellow'
+}
+export function messangerReducer(state,action){
+    switch(action.type){
+        case '':{
+
+        }
+        case '':{
+
+        }
+        default:{
+            
+        }
+
+    }
+}
