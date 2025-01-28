9852a2eb0d09adfc25a01f3226bf21c49cd5c800
Basit Ali <opoali911@gmail.com>
2025-01-28 18:43:18 +0500

    now working on the styling of this projcet

--- miniProjects/src/Projects/miniProject21/Task.jsx
+++ miniProjects/src/Projects/miniProject21/Task.jsx
@@ -5,11 +5,13 @@ import TaskList from "../../TaskList";
 
 const Task = () => {
   return (
-    <TasksProvider>
+  <div className="h-screen w-screen flex justify-center items-center flex-col gap-4">
+      <TasksProvider >
       <h1 className="text-3xl font-bold ">Day of in Skardu</h1>
       <AddTask />
       <TaskList/>
     </TasksProvider>
+  </div>
   );
 };
 
