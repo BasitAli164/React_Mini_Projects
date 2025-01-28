28aed99c7565f9c5a139961de90441238f72ef91
Basit Ali <opoali911@gmail.com>
2025-01-28 18:37:10 +0500

    know checking

--- miniProjects/src/TaskList.jsx
+++ miniProjects/src/TaskList.jsx
@@ -1,101 +1,99 @@
-import React, { useState } from 'react'
-import { useDispatchContext, useTasksContext } from './Projects/miniProject21/TaskContext'
+import React, { useState } from "react";
+import {
+  useDispatchContext,
+  useTasksContext,
+} from "./Projects/miniProject21/TaskContext";
 
 const TaskList = () => {
-    const tasks=useTasksContext()
+  const tasks = useTasksContext();
   return (
     <>
-    <ul>
-        {
-            tasks.map((item)=>(
-                <li key={item.id}>
-                    <Tasks task={item}/>
-
-                </li>
-            ))
-        }
-    </ul>
-      
+      <ul>
+        {tasks.map((item) => (
+          <li key={item.id}>
+            <Tasks task={item} />
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
 
-function Tasks({task}){
-    const [isEditing,setIsEditing]=useState(false);
-    const dispatch=useDispatchContext();
-    let taskContent;
-    if(isEditing){
-        taskContent=(
-            <>
-            <input
-             type="text"
-             value={task.text}
-             onChange={e=>{
-                dispatch({
-                    type:'changed',
-                    task:{
-                        ...task,
-                        text:e.target.value
-                    }
-                })
-             }}
-             className='bordre-[3px] border-solid border-black '
-             />
-             <button
-              onClick={()=>setIsEditing(false)}
-              className='mx-2 px-8 py-2 rounded-full bg-yellow-300'
-              >Save</button>
-            
-            </>
-        )
-
-    }else{
-        taskContent=(
-            <>
-            {task.text}
-            <button
-            onClick={()=>setIsEditing(true)}
-            className='px-8 py-2 mx-2 rounded-full bg-cyan-300'
-            >Edit</button>
-            </>
-        )
-    }
-
-    return(
-        <>
-        <label>
-        <input 
-        type="checkbox"
-        checked={task.done}
-        onChange={e=>{
+function Tasks({ task }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const dispatch = useDispatchContext();
+  let taskContent;
+  if (isEditing) {
+    taskContent = (
+      <>
+        <input
+          type="text"
+          value={task.text}
+          onChange={(e) => {
             dispatch({
-                type:'changed',
-                task:{
-                    ...task,
-                    done:e.target.checked
+              type: "changed",
+              task: {
+                ...task,
+                text: e.target.value,
+              },
+            });
+          }}
+          className="bordre-[3px] border-solid border-black "
+        />
+        <button
+          onClick={() => setIsEditing(false)}
+          className="mx-2 px-8 py-2 rounded-full bg-yellow-300"
+        >
+          Save
+        </button>
+      </>
+    );
+  } else {
+    taskContent = (
+      <>
+        {task.text}
+        <button
+          onClick={() => setIsEditing(true)}
+          className="px-8 py-2 mx-2 rounded-full bg-cyan-300"
+        >
+          Edit
+        </button>
+      </>
+    );
+  }
 
-                }
-            })
-        }}
-        className='border-[3px] border-black border-solid'
-
-         />
+  return (
+    <>
+      <label>
+        <input
+          type="checkbox"
+          checked={task.done}
+          onChange={(e) => {
+            dispatch({
+              type: "changed",
+              task: {
+                ...task,
+                done: e.target.checked,
+              },
+            });
+          }}
+          className="border-[3px] border-black border-solid"
+        />
         {taskContent}
         <button
-        onClick={()=>{
+          onClick={() => {
             dispatch({
-                type:'deleted',
-                id:task.id
-            })
-        }}
-        className='px-8 py-2 mx-2 rounded-full bg-red-500'
-        >Delete</button>
-        </label>
-       
-        </>
-
-    )
-
+              type: "deleted",
+              id: task.id,
+            });
+          }}
+          className="px-8 py-2 mx-2 rounded-full bg-red-500"
+        >
+          Delete
+        </button>
+      </label>
+    </>
+  );
 }
