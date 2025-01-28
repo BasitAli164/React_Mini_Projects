f6957db0edcdc82e6a1fae1d0d72423649a2fe61
Basit Ali <opoali911@gmail.com>
2025-01-28 18:15:51 +0500

    working on the todo like app

--- miniProjects/src/Projects/miniProject21/Task.jsx
+++ miniProjects/src/Projects/miniProject21/Task.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { TasksProvider } from './TaskContext'
-import AddTask from './AddTask'
+import React from "react";
+import { TasksProvider } from "./TaskContext";
+import AddTask from "./AddTask";
 
 const Task = () => {
   return (
     <TasksProvider>
-        <h1 className='text-3xl font-bold '>Day of in Skardu</h1>
-        <AddTask/>
+      <h1 className="text-3xl font-bold ">Day of in Skardu</h1>
+      <AddTask />
     </TasksProvider>
-  )
-}
+  );
+};
 
-export default Task
+export default Task;
