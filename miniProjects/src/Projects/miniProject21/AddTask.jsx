8ae66293268a6f58b2466c5855469109654f3c39
Basit Ali <opoali911@gmail.com>
2025-01-28 18:15:27 +0500

    the add task component is ready

---
+++ miniProjects/src/Projects/miniProject21/AddTask.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { useDispatchContext } from './TaskContext';
+import { nextId } from './reducer&initailState';
+
+const AddTask = () => {
+    const [text,setText]=useState('');
+    const dispatch=useDispatchContext();
+  return (
+    <>
+    <input
+     type="text"
+     placeholder='Add task'
+     value={text}
+     onChange={e=>setText(e.target.value)}
+     className='border-[2px] border-solid border-b-violet-700 focus:outline-none'
+
+     />
+     <button
+     className='px-8 py-2 mx-2 rounded-full bg-cyan-500'
+     onClick={()=>{
+        setText('')
+        dispatch({
+            type:'added',
+            id:nextId++,
+            text:text
+        })
+     }}>Add</button>
+      
+    </>
+  )
+}
+
+export default AddTask
--- miniProjects/src/Projects/miniProject21/Task.jsx
+++ miniProjects/src/Projects/miniProject21/Task.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { TasksProvider } from './TaskContext'
+import AddTask from './AddTask'
 
 const Task = () => {
   return (
     <TasksProvider>
         <h1 className='text-3xl font-bold '>Day of in Skardu</h1>
-        
-
+        <AddTask/>
     </TasksProvider>
   )
 }
