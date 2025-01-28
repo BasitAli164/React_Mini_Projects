4979214edbb21876cdae084c7182869f3fafd444
Basit Ali <opoali911@gmail.com>
2025-01-28 18:37:43 +0500

    know see in borweres

--- miniProjects/src/Projects/miniProject21/Task.jsx
+++ miniProjects/src/Projects/miniProject21/Task.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { TasksProvider } from "./TaskContext";
 import AddTask from "./AddTask";
+import TaskList from "../../TaskList";
 
 const Task = () => {
   return (
     <TasksProvider>
       <h1 className="text-3xl font-bold ">Day of in Skardu</h1>
       <AddTask />
+      <TaskList/>
     </TasksProvider>
   );
 };
