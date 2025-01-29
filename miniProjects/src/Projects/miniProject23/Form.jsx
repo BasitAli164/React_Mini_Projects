c323877cb8480910d2c5f66586571d2be0d2ce48
Basit Ali <opoali911@gmail.com>
2025-01-29 18:35:21 +0500

    start my project on local storage

--- miniProjects/src/Projects/miniProject23/Form.jsx
+++ miniProjects/src/Projects/miniProject23/Form.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 
 const Form = () => {
-    const [toggle,setToggle]=useState(false)
+    const [toggle,setToggle]=useState(false);
+    const handleSubmit=()=>{
+        
+    }
   return (
     <>
     <h1>{toggle?'Sign Up':'Login'} Form</h1>
