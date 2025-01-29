aee1e405e9529e9112d72ef1c1e82f27326ec5de
Basit Ali <opoali911@gmail.com>
2025-01-29 18:32:45 +0500

    know start the making the basic structure of the

--- miniProjects/src/Projects/miniProject23/Form.jsx
+++ miniProjects/src/Projects/miniProject23/Form.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+    const [toggle,setToggle]=useState(false)
   return (
     <>
-    
+    <h1>{toggle?'Sign Up':'Login'} Form</h1>
+    <form onSubmit={handleSubmit}>
+        <div>
+            <label>userName</label>
+            <input type="text" />
+        </div>
+
+    </form>
       
     </>
   )
