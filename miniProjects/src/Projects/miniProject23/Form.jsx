63e134d073df493c9649a076fa63ee486d7ce280
Basit Ali <opoali911@gmail.com>
2025-01-29 18:37:22 +0500

    the basic structure is ready

--- miniProjects/src/Projects/miniProject23/Form.jsx
+++ miniProjects/src/Projects/miniProject23/Form.jsx
@@ -3,15 +3,23 @@ import React, { useState } from 'react'
 const Form = () => {
     const [toggle,setToggle]=useState(false);
     const handleSubmit=()=>{
-        
+
     }
   return (
     <>
     <h1>{toggle?'Sign Up':'Login'} Form</h1>
     <form onSubmit={handleSubmit}>
         <div>
-            <label>userName</label>
+            <label>userName:</label>
             <input type="text" />
+        </div>
+        <div>
+            <label>email:</label>
+            <input type="email" />
+        </div>
+        <div>
+            <label>password:</label>
+            <input type="password" />
         </div>
 
     </form>
