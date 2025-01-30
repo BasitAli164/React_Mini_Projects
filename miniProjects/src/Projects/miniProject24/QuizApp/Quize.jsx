1961f26d2493e0fa1249c868a6c1ab2490d38c28
Basit Ali <opoali911@gmail.com>
2025-01-30 22:13:01 +0500

    working on the stylin of the quize app

--- miniProjects/src/Projects/miniProject24/QuizApp/Quize.jsx
+++ miniProjects/src/Projects/miniProject24/QuizApp/Quize.jsx
@@ -4,7 +4,7 @@ const Quize = () => {
   return (
     <div className='w-[640px] m-auto  bg-white text-[#262626] flex flex-col rounded-[10px] py-[40px] px-[50px]'>
         <h1>Quize App</h1>
-        <hr />
+        <hr  />
         <h2>Which Device is required for the Internet connection?</h2>
         <ul>
             <li>Modem</li>
@@ -12,7 +12,7 @@ const Quize = () => {
             <li>LAN Cable</li>
             <li>Pen Drive</li>
         </ul>
-        <button>Next</button>
+        <button className='px-6 py-1 bg-stone-500 rounded-sm'>Next</button>
         <div>1 of 5 questions</div>
 
       
