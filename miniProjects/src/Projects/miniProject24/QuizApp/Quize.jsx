6c7c4cfd849fdb4d3e1ff58e5cda18c64fe9f94a
Basit Ali <opoali911@gmail.com>
2025-01-31 22:44:55 +0500

    know data are comes dynamically in the quize app

--- miniProjects/src/Projects/miniProject24/QuizApp/Quize.jsx
+++ miniProjects/src/Projects/miniProject24/QuizApp/Quize.jsx
@@ -1,25 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "./Data";
 
 const Quize = () => {
+    const [index,setIndex]=useState(0);
+    const [question,setQuestion]=useState(data[index])
   return (
     <div className="w-[640px] m-auto  bg-white text-[#262626] flex flex-col rounded-[10px] py-[40px] px-[50px]">
       <h1 className="text-xl mb-1 font-bold">Quize App</h1>
       <hr className="h-[2px] border-none bg-[#707070]" />
       <h2 className="text-[25px] font-semibold ">
-        1. Which Device is required for the Internet connection?
+        {index+1}. {question.question}
       </h2>
       <ul className="mt-5">
         <li className="flex  items-center h-[50px] pl-[20px] border border-solid border-[#686868] rounded-[8px] mb-[15px] text-[20px] cursor-pointer">
-          Modem
+          {question.option1}
         </li>
         <li className="flex  items-center h-[50px] pl-[20px] border border-solid border-[#686868] rounded-[8px] mb-[15px] text-[20px] cursor-pointer">
-          Router
+          {question.option2}
         </li>
         <li className="flex  items-center h-[50px] pl-[20px] border border-solid border-[#686868] rounded-[8px] mb-[15px] text-[20px] cursor-pointer">
-          LAN Cable
+          {question.option3}
         </li>
         <li className="flex  items-center h-[50px] pl-[20px] border border-solid border-[#686868] rounded-[8px] mb-[15px] text-[20px] cursor-pointer">
-          Pen Drive
+          {question.option4}
         </li>
       </ul>
       <button className="px-6 py-1 bg-[#553f9a] rounded-[8px] m-auto w-[300px] h-[60px] text-[#fff] font-semibold cursor-pointer text-[25px]">
