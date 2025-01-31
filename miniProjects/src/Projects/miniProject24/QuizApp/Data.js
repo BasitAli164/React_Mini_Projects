8d43acfcc4987dcc16bb56404b1cf2a60e758aa5
Basit Ali <opoali911@gmail.com>
2025-01-31 22:38:34 +0500

    data is ready

--- miniProjects/src/Projects/miniProject24/QuizApp/Data.js
+++ miniProjects/src/Projects/miniProject24/QuizApp/Data.js
@@ -1,10 +1,43 @@
-export const data=[
+export const data = [
     {
-        question:" Which Device is required for the Internet connection ?",
-        option1:"Modem",
-        option2:"Router",
-        option3:"LAN Cable",
-        option4:"Pen Drive",
-        ans:1,
-    }
-]
+      question: "Which Device is required for the Internet connection?",
+      option1: "Modem",
+      option2: "Router",
+      option3: "LAN Cable",
+      option4: "Pen Drive",
+      ans: 1,
+    },
+    {
+      question: "Which protocol is used to browse websites?",
+      option1: "SMTP",
+      option2: "HTTP",
+      option3: "FTP",
+      option4: "IP",
+      ans: 2,
+    },
+    {
+      question: "What does CPU stand for?",
+      option1: "Central Process Unit",
+      option2: "Computer Personal Unit",
+      option3: "Central Processing Unit",
+      option4: "Central Performance Unit",
+      ans: 3,
+    },
+    {
+      question: "Which of the following is a programming language?",
+      option1: "HTML",
+      option2: "CSS",
+      option3: "Python",
+      option4: "HTTP",
+      ans: 3,
+    },
+    {
+      question: "What does RAM stand for?",
+      option1: "Read Access Memory",
+      option2: "Random Access Memory",
+      option3: "Run Access Memory",
+      option4: "Remote Access Memory",
+      ans: 2,
+    },
+  ];
+  
