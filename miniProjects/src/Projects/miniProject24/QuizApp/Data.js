1ad09061ec38ffaaf5dd2b867159aa0146ff02b8
Basit Ali <opoali911@gmail.com>
2025-01-31 22:35:24 +0500

    i data is ready

--- miniProjects/src/Projects/miniProject24/QuizApp/Data.js
+++ miniProjects/src/Projects/miniProject24/QuizApp/Data.js
@@ -1,5 +1,10 @@
 export const data=[
     {
-        question:" Which Device is required for the Internet connection ?"
+        question:" Which Device is required for the Internet connection ?",
+        option1:"Modem",
+        option2:"Router",
+        option3:"LAN Cable",
+        option4:"Pen Drive",
+        ans:1,
     }
 ]
