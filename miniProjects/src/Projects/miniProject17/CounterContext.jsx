1b9f5f3525a8824bd1c298ecab1866fa6340bf0c
Basit Ali <opoali911@gmail.com>
2025-02-02 23:51:03 +0500

    now makin the provider

--- miniProjects/src/Projects/miniProject17/CounterContext.jsx
+++ miniProjects/src/Projects/miniProject17/CounterContext.jsx
@@ -1,4 +1,11 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const counterContext=createContext();
 
+const CounterContext=({children})=>{
+    const [count,setCount]=useState(0)
+    const increment=()=>{
+        setCount(count+1)
+    }
+    const
+}
