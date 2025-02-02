075fc9dddd803f609b71593641591a239fdd3588
Basit Ali <opoali911@gmail.com>
2025-02-02 23:24:26 +0500

    almost finished the counter component

--- miniProjects/src/Projects/miniProject16/Counter.jsx
+++ miniProjects/src/Projects/miniProject16/Counter.jsx
@@ -5,6 +5,9 @@ const Counter = () => {
     const {count, increment,decrement}=useContext(counterContext)
   return (
     <div>
+        <button onClick={increment} className='mx-3 py-1 px-8 bg-cyan-600 rounded-full'>+</button>
+        <span>{count}</span>
+        <button onClick={decrement} className='mx-3 py-1 px-8 bg-cyan-600 rounded-full'>-</button>
       
     </div>
   )
