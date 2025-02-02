4e9eb289d6b479c717fa44ba2cfc806d0cce193c
Basit Ali <opoali911@gmail.com>
2025-02-02 23:19:10 +0500

    working on the provder

--- miniProjects/src/Projects/miniProject16/CounterProvider.jsx
+++ miniProjects/src/Projects/miniProject16/CounterProvider.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { counterContext } from './counterContext'
 
 const CounterProvider = () => {
+    const [count,setCount]=useState(0)
+    const increment=()=>{
+        setCount(count+1)
+    }
+    const decrement=()=>{
+        setCount(count-1)
+    }
   return (
     <div>
       
