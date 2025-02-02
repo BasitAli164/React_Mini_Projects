87d1b88aa6c561212e50ea1fe2f7cbfef2c43170
Basit Ali <opoali911@gmail.com>
2025-02-02 23:52:35 +0500

    almost finish the provider

--- miniProjects/src/Projects/miniProject17/CounterContext.jsx
+++ miniProjects/src/Projects/miniProject17/CounterContext.jsx
@@ -2,10 +2,17 @@ import { createContext, useState } from "react";
 
 const counterContext=createContext();
 
-const CounterContext=({children})=>{
+export const CounterProvider=({children})=>{
     const [count,setCount]=useState(0)
     const increment=()=>{
         setCount(count+1)
     }
-    const
+    const decrement=()=>{
+        setCount(count-1)
+    }
+    return(
+        <counterContext.Provider>
+            {children}
+        </counterContext.Provider>
+    )
 }
