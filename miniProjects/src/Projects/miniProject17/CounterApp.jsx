c69e11db9f023c75330978cf315d4bac2c08daf9
Basit Ali <opoali911@gmail.com>
2025-02-02 23:59:46 +0500

    so finally understart the working of the context

--- miniProjects/src/Projects/miniProject17/CounterApp.jsx
+++ miniProjects/src/Projects/miniProject17/CounterApp.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import { useCounter } from './CounterContext'
+import React from "react";
+import { useCounter } from "./CounterContext";
 
 const CounterApp = () => {
-    const {count,increment,decrement}=useCounter();
+  const { count, increment, decrement } = useCounter();
 
   return (
     <div>
-        <button className='px-10 py-2 mx-3 bg-cyan-400 rounded-full' onClick={increment}>+</button>
-        <span className='font-bold text-white text-4xl mx-2'>{count}</span>
-        <button className='px-10 py-2 mx-3 bg-cyan-400 rounded-full' onClick={decrement}>-</button>
-      
+      <button
+        className="px-10 py-2 mx-3 bg-cyan-400 rounded-full"
+        onClick={increment}
+      >
+        +
+      </button>
+      <span className="font-bold text-white text-4xl mx-2">{count}</span>
+      <button
+        className="px-10 py-2 mx-3 bg-cyan-400 rounded-full"
+        onClick={decrement}
+      >
+        -
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default CounterApp
+export default CounterApp;
