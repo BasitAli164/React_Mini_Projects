13671642bc3c113b79cbae062d7f39f70f2753a1
Basit Ali <opoali911@gmail.com>
2025-02-03 14:04:47 +0500

    i also make the my  hook

--- miniProjects/src/Projects/miniProject18/ThemeContext.jsx
+++ miniProjects/src/Projects/miniProject18/ThemeContext.jsx
@@ -1,14 +1,14 @@
 import { createContext, useContext, useState } from "react";
-const themeContext=createContext();
+const themeContext = createContext();
 
-export const ThemeProvider=({children})=>{
-    const [theme,setTheme]=useState('lightblue')
-    return(
-        <themeContext.Provider value={{theme,setTheme}}>
-            {children}
-        </themeContext.Provider>
-    )
-}
-export const useTheme=()=>{
-    return useContext(themeContext)
-}
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("lightblue");
+  return (
+    <themeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </themeContext.Provider>
+  );
+};
+export const useTheme = () => {
+  return useContext(themeContext);
+};
