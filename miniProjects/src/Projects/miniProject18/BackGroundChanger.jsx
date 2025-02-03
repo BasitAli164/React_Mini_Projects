2faef5d7c069d94bac1d4b566eeaa99f252247bc
Basit Ali <opoali911@gmail.com>
2025-02-03 14:09:10 +0500

    know checking

--- miniProjects/src/Projects/miniProject18/BackGroundChanger.jsx
+++ miniProjects/src/Projects/miniProject18/BackGroundChanger.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
-import { useTheme } from './ThemeContext'
+import React from "react";
+import { useTheme } from "./ThemeContext";
 
 const BackGroundChanger = () => {
-    const{theme,setTheme}=useTheme();
+  const { theme, setTheme } = useTheme();
   return (
-    <section className={`h-full w-full bg-[${theme}] flex justify-center items-center`}>
-        <button className='px-10 py-2 rounded-full bg-red-600' onClick={()=>setTheme('black')}>Click</button>      
+    <section
+      className={`h-full w-full bg-[${theme}] flex justify-center items-center`}
+    >
+      <button
+        className="px-10 py-2 rounded-full bg-red-600"
+        onClick={() => setTheme("black")}
+      >
+        Click
+      </button>
     </section>
-  )
-}
+  );
+};
 
-export default BackGroundChanger
+export default BackGroundChanger;
