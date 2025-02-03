8cceb67e56a41bd4a9109404a97d3944990a08ba
Basit Ali <opoali911@gmail.com>
2025-02-03 17:07:38 +0500

    working on the reducer

---
+++ miniProjects/src/Projects/miniProject21/reducer&initailState.js
@@ -0,0 +1,19 @@
+export const nextId=2
+export const tasksInitialState =[
+    {id:0,text:"Philosopher's Path",done:true},
+    {id:1,text:"Reading Books Daily",done:false},
+]
+export const taskReducer=(task,action)=>{
+    switch(action.type){
+        case 'added':{
+            return [
+                ...task,
+                {
+                    id:action.id,
+                    text:action.text,
+                    done:false
+                }
+            ]
+        }
+    }
+}
