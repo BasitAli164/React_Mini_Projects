db86c1b4824a0b8868336ff3e0dfe2ee56377d81
Basit Ali <opoali911@gmail.com>
2025-02-03 17:00:33 +0500

    working on the large project

--- miniProjects/src/Projects/miniProject21/TaskContext.jsx
+++ miniProjects/src/Projects/miniProject21/TaskContext.jsx
@@ -1,3 +1,23 @@
 import { createContext,useContext,useReducer } from "react";
 
-const TaskContext=createContext(null);
+const TasksContext=createContext(null);
+const TasksDispatchContext=createContext(null);
+
+export const TasksProvider=({children})=>{
+    const [tasks,dispatch]=useReducer(taskReducer,tasksInitialState);
+
+    return(
+        <TasksContext.Provider value={tasks}>
+            <TasksDispatchContext.Provider value={dispatch}>
+                {children}
+            </TasksDispatchContext.Provider>
+        </TasksContext.Provider>
+    )
+}
+
+export const useTasksContext=()=>{
+    return useContext(TasksContext)
+}
+export const useDispatchContext=()=>{
+    return useContext(TasksDispatchContext)
+}
