dc0b6a252d8e29c2fb32ec5a1f0e985f3355dba1
Basit Ali <opoali911@gmail.com>
2025-02-06 23:22:57 +0500

    know working on the chat gpt clone project

---
+++ miniProjects/src/Projects/miniProject25/Clone/Chat.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <>
+    <div className="App">
+        <div className="sidebar">
+            
+        </div>
+        <div className="main">
+
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default Chat
