4162bcad5bad9d659b64408730cb95368e6abbc2
Basit Ali <opoali911@gmail.com>
2025-02-07 09:14:04 +0500

    working on the clone of the gpt

--- miniProjects/src/Projects/miniProject25/Clone/Chatgpt.jsx
+++ miniProjects/src/Projects/miniProject25/Clone/Chatgpt.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import gptLogo from '../assets/gptlogo2.svg'
-import {Plus} from 'react-feather'
+import gptLogo from '../assets/gptlogo1.jpg'
+import {Plus,MessageSquare} from 'react-feather'
 
 const Chatgpt = () => {
   return (
@@ -9,14 +9,14 @@ const Chatgpt = () => {
         <div className="sidebar w-[300px] border-r-4 border-solid border-[rgb(110,108,108)]">
           <div className="upperSide p-5">
             <div className="upperSideTop flex flex-col gap-y-3">
-              <div className='flex items-center ml-2'>
-              <img src={gptLogo} alt="GPT-Logo" className="w-20" />
+              <div className='flex items-center ml-2 gap-4'>
+              <img  src={gptLogo} alt="GPT-Logo" className="w-20 rounded-full " />
               <span className="brand text-3xl font-bold">ChatGPT</span>
               </div>
-              <button className="midbtn px-4 py-4 ml-2 rounded-md bg-purple-400 flex justify-center text-4xl"><Plus/>New Chat</button>
-              <div className="upperSideBottom">
-                <button className="query"><img src="" alt="" />What is Programming ?</button>
-                <button className="query"><img src="" alt="" />What is Programming ?</button>
+              <button className="midbtn px-4 py-3 ml-2 rounded-md bg-purple-400 flex justify-center text-2xl items-center gap-2"><Plus/>New Chat</button>
+              <div className="upperSideBottom ml-2 mt-5">
+                <button className="query flex px-3 py-2 rounded-md border-[1px] border-dotted my-4  "><MessageSquare/><span className='mx-2'>What is Programming ?</span></button>
+                <button className="query flex px-8 py-2 rounded-md border-[1px] border-dotted my-4  "><MessageSquare/><span className='mx-2'>How to use API ?</span></button>
               </div>
             </div>
           </div>
