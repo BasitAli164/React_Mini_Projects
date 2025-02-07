be9fcba019bddd3b64f2d8ba6ceaa24970935fb2
Basit Ali <opoali911@gmail.com>
2025-02-07 08:54:46 +0500

    working on the gpt clone

--- miniProjects/src/Projects/miniProject25/Clone/Chatgpt.jsx
+++ miniProjects/src/Projects/miniProject25/Clone/Chatgpt.jsx
@@ -5,13 +5,15 @@ import {Plus} from 'react-feather'
 const Chatgpt = () => {
   return (
     <>
-    <div className="App bg-[rgb(3,0,31)] text-white min-h-screen w-screen flex">
-        <div className="sidebar flex-3 border-r-4">
-          <div className="upperSide">
-            <div className="upperSideTop">
+    <div className="App bg-[rgb(3,0,31)] text-white min-h-screen w-screen flex ">
+        <div className="sidebar w-[300px] border-r-4 border-solid border-[rgb(110,108,108)]">
+          <div className="upperSide p-5">
+            <div className="upperSideTop flex flex-col gap-y-3">
+              <div className='flex items-center ml-2'>
               <img src={gptLogo} alt="GPT-Logo" className="w-20" />
-              <span className="brand">ChatGPT</span>
-              <button className="midbtn"><Plus/>New Chat</button>
+              <span className="brand text-3xl font-bold">ChatGPT</span>
+              </div>
+              <button className="midbtn px-4 py-4 ml-2 rounded-md bg-purple-400 flex justify-center text-4xl"><Plus/>New Chat</button>
               <div className="upperSideBottom">
                 <button className="query"><img src="" alt="" />What is Programming ?</button>
                 <button className="query"><img src="" alt="" />What is Programming ?</button>
